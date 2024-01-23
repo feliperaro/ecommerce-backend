29346a747d6500d9e8d7fcfd712bc035f139ad9d
FehRoque <feliperamosroque@gmail.com>
2024-01-23 12:20:52 -0300

    GroupSerializer

--- ecommerce/ecommerce/api/serializers.py
+++ ecommerce/ecommerce/api/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 
@@ -7,3 +7,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
