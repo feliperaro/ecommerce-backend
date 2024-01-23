3887e824f7c6ebed17bb64bb4c2c208b77ebc3ac
FehRoque <feliperamosroque@gmail.com>
2024-01-23 12:24:25 -0300

    groups into urls

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -22,6 +22,7 @@ from ecommerce.api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
